b5aeaa4cd49a694168c444144e06ed1abe4a3db6
ZachHoskins <zach9414@gmail.com>
2019-11-12 05:15:03 -0600

    removed build\ from .gitignore

---
+++ build/precache-manifest.afb8162e70edb97af81370c5a41d927b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72ff0afc578267f8b5527c8da3f6a33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d17478a4f66f2fc3cbf",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "34ce4d27f6afe5e44383",
+    "url": "/static/css/main.20034467.chunk.css"
+  },
+  {
+    "revision": "6d17478a4f66f2fc3cbf",
+    "url": "/static/js/2.b67e0a45.chunk.js"
+  },
+  {
+    "revision": "34ce4d27f6afe5e44383",
+    "url": "/static/js/main.0e8ca53c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
